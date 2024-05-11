a8e622e49e040b52bd8f930f3878a886ead848ca
Sidd-77 <siddharthsalunkhe2003@gmail.com>
2024-05-12 01:58:50 +0530

    shortt

---
+++ main.py
@@ -0,0 +1,54 @@
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_chroma import Chroma
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+llm = ChatOpenAI(model='gpt-3.5-turbo-0125', api_key=os.getenv('OPENAI_API_KEY'))
+
+
+loader = WebBaseLoader(
+    web_path='https://lilianweng.github.io/posts/2023-06-23-agent/',
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+retrieved_docs = retriever.invoke("What are the approaches to Task Decomposition?")
+
+
+prompt = hub.pull("rlm/rag-prompt")
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+for chunk in rag_chain.stream("What is Task Decomposition?"):
+    print(chunk, end="", flush=True)
+
